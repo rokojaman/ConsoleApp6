edabc53d104ec8570229925f2868b6d0cd82ce58
ajuric05 <115616326+ajuric05@users.noreply.github.com>
2024-03-18 20:40:34 +0100

    Update Program.cs
    komentar

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!!!!!!!!!!!!!!!!!!");
 Console.WriteLine("novonov");
+
+
+//komentar od kolege Antonia  Jurića-Pešića
